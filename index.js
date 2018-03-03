760b42d8d24d8340655aa887c76869e1a20141db
Dhanush Patel <dhanush.patel@ymail.com>
2018-03-03 14:07:53 -0800

    revert lol

--- index.js
+++ index.js
@@ -4,28 +4,13 @@ const VoiceResponse = require('twilio').twiml.VoiceResponse;
 module.exports = function (context, req) {
   context.log('triggered...');
 
-//  const formValues = qs.parse(req.body);
-//  // Insert spaces between numbers to aid text-to-speech engine
-//  const phoneNumber = formValues.From.replace(/\+/g, '').split('').join(' ');
-//
-//  const twiml = new VoiceResponse();
-//  console.log("phoneNumber",phoneNumber);
-//  twiml.say('Your phone number is: ' + phoneNumber);
-//
-//  context.res = {
-//    status: 200,
-//    body: twiml.toString(),
-//    headers: { 'Content-Type': 'application/xml' },
-//    isRaw: true
-//  };
-  
+  const formValues = qs.parse(req.body);
+  // Insert spaces between numbers to aid text-to-speech engine
+  const phoneNumber = formValues.From.replace(/\+/g, '').split('').join(' ');
+
   const twiml = new VoiceResponse();
-  const gather = twiml.gather({
-    input: 'speech dtmf',
-    timeout: 3,
-    numDigits: 1,
-  });
-  gather.say('Please press 1 or say sales for sales.');
+  console.log("phoneNumber",phoneNumber);
+  twiml.say('Your phone number is: ' + phoneNumber);
 
   context.res = {
     status: 200,
@@ -33,5 +18,6 @@ module.exports = function (context, req) {
     headers: { 'Content-Type': 'application/xml' },
     isRaw: true
   };
+  
   context.done();
 };
