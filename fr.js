9eec5683ca55d2cfc98d4e157958d65897c7393e
josephcsu <37024394+josephcsu@users.noreply.github.com>
2018-03-04 06:09:53 -0800

    Update fr.js

--- fr.js
+++ fr.js
@@ -1,6 +1,26 @@
-exports.handler = function(context, event, callback) {
-	const twiml = new Twilio.twiml.VoiceResponse();
-  	twiml.say('Salut tout le monde', {language: 'fr'});
-
-  	callback(null, twiml);
+exports.handler = function(context, event, callback) {  
+  const twiml = new Twilio.twiml.VoiceResponse();
+ 
+  if (event.Digits) {
+    switch (event.Digits) {
+      case '1':
+        twiml.say('Vous écoutez maintenant des nouvelles. Appuyez sur n\'importe quelle touche pour ignorer l\'article en cours.', {language: 'fr'});
+        twiml.redirect('/fr-news');
+        break;
+      case '2':
+        twiml.say('Vous êtes en train d\'écouter du sport.', {language: 'fr'});
+        twiml.redirect('/fr-sports');
+        break;
+      default:
+        twiml.say("Désolé, je ne comprends pas ce choix.");
+        twiml.pause({length:2});
+        twiml.redirect('/get-response');
+        break;
+    }
+  }
+  else {
+    twiml.redirect('/get-response');
+  }
+  
+  callback(null, twiml);
 };
