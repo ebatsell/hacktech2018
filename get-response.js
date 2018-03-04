0eb4b8808546bb0eb02f6f546ea4c6ece7b5a30f
josephcsu <37024394+josephcsu@users.noreply.github.com>
2018-03-03 20:15:33 -0800

    Update get-response.js

--- get-response.js
+++ get-response.js
@@ -1,9 +1,27 @@
 exports.handler = function(context, event, callback) {
   const twiml = new Twilio.twiml.VoiceResponse();
  
-  const command = event.SpeechResult.toLowerCase();
-  twiml.say(`You said ${command}.`);
+  if (event.Digits) {
+    switch (event.Digits) {
+      case '1':
+        twiml.say('You selected English.');
+        break;
+      case '2':
+        twiml.say('You selected Spanish.');
+        break;
+      case '3':
+        twiml.say('You selected French.');
+        break;
+      default:
+        twiml.say("Sorry, I don't understand that choice.").pause();
+        twiml.redirect('/main');
+        break;
+    }
+  } else {
+    // If no input was sent, redirect to the main menu
+    twiml.redirect('/main');
+  }
+  //twiml.say(`You said ${command}.`);
   
   callback(null, twiml);
 };
-
