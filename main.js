4a0f79d6bcc0557596f6871b6130e2bda24d5fe1
josephcsu <37024394+josephcsu@users.noreply.github.com>
2018-03-03 20:14:20 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 exports.handler = function(context, event, callback) {
   const twiml = new Twilio.twiml.VoiceResponse();
  
-  twiml.gather({
-    input: 'speech',
+  const gather = twiml.gather({
+    input: 'dtmf',
     timeout: 3,
+    numDigits: 1,
     action: '/get-response'
-  }).say('Welcome to Hacktech 2018. Please list your symptoms.');
+  }).say('Welcome to Hacktech 2018. Press ,1, for English,', {language: 'en'});
   
+  twiml.say('Welcome to Hacktech 2018. Press ,1, for English,', {language: 'en'});
+  twiml.say('Press ,2, for Español,', {language: 'es'});
+  twiml.say('or press ,3, for Français,', {language: 'fr'});
+  twiml.say('Press 0 at any time to hang up.', {language: 'en'});
+  
+  //twiml.say(gather);
  
   callback(null, twiml);
 };
