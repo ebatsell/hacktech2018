7b42520c1d54396c74b71fd4ea5ec9a316490b03
josephcsu <37024394+josephcsu@users.noreply.github.com>
2018-03-04 06:07:29 -0800

    Update get-response.js

--- get-response.js
+++ get-response.js
@@ -1,19 +1,37 @@
-exports.handler = function(context, event, callback) {
+exports.handler = function(context, event, callback) {  
   const twiml = new Twilio.twiml.VoiceResponse();
  
   if (event.Digits) {
     switch (event.Digits) {
       case '1':
-        twiml.say('You selected English.', {language: 'en'});
-        twiml.redirect('/en');
+        gather = twiml.gather({
+			input: 'dtmf',
+			timeout: 1,
+			numDigits: 1,
+			action: '/en'
+		}).say('You selected English. Press ,1, to listen to news or, press ,2, to listen to sports.', {language: 'en'});
+        twiml.pause({length:2});
+        twiml.redirect('/main');
         break;
       case '2':
-        twiml.say('Has seleccionado Español.', {language: 'es'});
-        twiml.redirect('/es');
+        gather = twiml.gather({
+			input: 'dtmf',
+			timeout: 1,
+			numDigits: 1,
+			action: '/es'
+		}).say('Has seleccionado Español. Presione ,1, para escuchar noticias o, presione ,2, para escuchar deportes.', {language: 'es'});
+        twiml.pause({length:2});
+        twiml.redirect('/main');
         break;
       case '3':
-        twiml.say('vous avez sélectionné Français.', {language: 'fr'});
-        twiml.redirect('/fr');
+        gather = twiml.gather({
+			input: 'dtmf',
+			timeout: 1,
+			numDigits: 1,
+			action: '/fr'
+		}).say('Vous avez sélectionné Français. Appuyez sur ,1, pour écouter les nouvelles ou appuyez sur ,2, pour écouter les sports.', {language: 'fr'});
+        twiml.pause({length:2});
+        twiml.redirect('/main');
         break;
       default:
         twiml.say("Sorry, I don't understand that choice.");
@@ -21,9 +39,8 @@ exports.handler = function(context, event, callback) {
         twiml.redirect('/main');
         break;
     }
-  } else {
-    twiml.redirect('/main');
   }
+  twiml.redirect('/main');
   
   callback(null, twiml);
 };
