15dc1601ae954556ce27a67f974cf3b1113058f6
josephcsu <37024394+josephcsu@users.noreply.github.com>
2018-03-03 20:40:03 -0800

    proper pause and gather

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
 exports.handler = function(context, event, callback) {
   const twiml = new Twilio.twiml.VoiceResponse();
- 
-  const gather = twiml.gather({
+  
+  gather = twiml.gather({
     input: 'dtmf',
-    timeout: 3,
+    timeout: 1,
     numDigits: 1,
     action: '/get-response'
   }).say('Welcome to Hacktech 2018. Press ,1, for English,', {language: 'en'});
   
-  twiml.say('Welcome to Hacktech 2018. Press ,1, for English,', {language: 'en'});
-  twiml.say('Press ,2, for Español,', {language: 'es'});
-  twiml.say('or press ,3, for Français,', {language: 'fr'});
-  twiml.say('Press 0 at any time to hang up.', {language: 'en'});
+  gather = twiml.gather({
+    input: 'dtmf',
+    timeout: 1,
+    numDigits: 1,
+    action: '/get-response'
+  }).say('Press ,2, for Español,', {language: 'es'});
   
-  //twiml.say(gather);
- 
+  gather = twiml.gather({
+    input: 'dtmf',
+    timeout: 1,
+    numDigits: 1,
+    action: '/get-response'
+  }).say('Press ,3, for Français,', {language: 'fr'});
+  
+  gather = twiml.gather({
+    input: 'dtmf',
+    timeout: 1,
+    numDigits: 1,
+    action: '/get-response'
+  }).say('Press 0 at any time to hang up.', {language: 'en'});
+  
+  twiml.redirect('/main');
+  
   callback(null, twiml);
 };
