fc08d334d580494510612b5286840b66180f7342
josephcsu <37024394+josephcsu@users.noreply.github.com>
2018-03-03 22:04:34 -0800

    article announce

--- en.js
+++ en.js
@@ -1,6 +1,31 @@
-exports.handler = function(context, event, callback) {
-	const twiml = new Twilio.twiml.VoiceResponse();
-  	twiml.say("Hello World", {language: 'en'});
+const request = require('request');
 
-  	callback(null, twiml);
-};
+function getQuote() {
+  return new Promise(function(resolve, reject) {
+    request('http://fa381518.ngrok.io/news?a=c', { json: true }, (err, res, body) => {
+      	resolve(body.articles);
+	});
+  });
+}
+function sleep(time) {
+	return new Promise((resolve) => setTimeout(resolve, time));  
+}
+const main = function (context, event, callback) {
+	const twiml = new Twilio.twiml.VoiceResponse();
+  	getQuote().then((articles) => {
+      	var arr = ["first", "second", "third", "fourth", "fifth"];
+      	for (var i = 0; i < articles.length; i++) {
+  			twiml.say("Your " + arr[i] + " article: " + articles[i].headline);
+          	twiml.pause({length: 1});
+          	//twiml.say(articles[i].article);
+          	//twiml.pause({length: 2});
+        }
+    });
+  	sleep(300).then(() => {
+  		callback(null, twiml);
+    });
+}
+
+exports.handler = main;
+
+
