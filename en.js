2a5ff9d11bf27961d986905d92c06b14caaa3820
josephcsu <37024394+josephcsu@users.noreply.github.com>
2018-03-04 06:08:58 -0800

    Update en.js

--- en.js
+++ en.js
@@ -1,31 +1,26 @@
-const request = require('request');
-
-function getQuote() {
-  return new Promise(function(resolve, reject) {
-    request('http://fa381518.ngrok.io/news?a=c', { json: true }, (err, res, body) => {
-      	resolve(body.articles);
-	});
-  });
-}
-function sleep(time) {
-	return new Promise((resolve) => setTimeout(resolve, time));  
-}
-const main = function (context, event, callback) {
-	const twiml = new Twilio.twiml.VoiceResponse();
-  	getQuote().then((articles) => {
-      	var arr = ["first", "second", "third", "fourth", "fifth"];
-      	for (var i = 0; i < articles.length; i++) {
-  			twiml.say("Your " + arr[i] + " article: " + articles[i].headline);
-          	twiml.pause({length: 1});
-          	//twiml.say(articles[i].article);
-          	//twiml.pause({length: 2});
-        }
-    });
-  	sleep(300).then(() => {
-  		callback(null, twiml);
-    });
-}
-
-exports.handler = main;
-
-
+exports.handler = function(context, event, callback) {  
+  const twiml = new Twilio.twiml.VoiceResponse();
+ 
+  if (event.Digits) {
+    switch (event.Digits) {
+      case '1':
+        twiml.say('You are now listening to news. Press any key to skip the current article.', {language: 'en'});
+        twiml.redirect('/en-news');
+        break;
+      case '2':
+        twiml.say('You are now listening to sports.', {language: 'en'});
+        twiml.redirect('/en-sports');
+        break;
+      default:
+        twiml.say("Sorry, I don't understand that choice.");
+        twiml.pause({length:2});
+        twiml.redirect('/get-response');
+        break;
+    }
+  }
+  else {
+    twiml.redirect('/get-response');
+  }
+  
+  callback(null, twiml);
+};
